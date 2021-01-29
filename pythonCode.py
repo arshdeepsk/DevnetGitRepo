de373591a96f36c5ddb039d0a2f7f12afb76a3e8
arshdeepsk <arshkawatra@gmail.com>
2021-01-30 00:07:40 +0530

    Added college name

--- pythonCode.py
+++ pythonCode.py
@@ -1,3 +1,3 @@
 
 for count in range(0,5):
-    print(str(count+1) + ". Arshdeep Singh Kawatra")
+    print(str(count+1) + ". Arshdeep Singh Kawatra -" + " Thapar Insitute of Engineering & Technology, Patiala, Punjab")
